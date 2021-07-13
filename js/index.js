316147e6aa36a63a3ac606ba88dcf074f43aa9e8
Macabee51 <devadvisorllc@gmail.com>
2021-07-13 16:49:45 -0700

    Making getRandomQuote function

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const quotes=[
+    {
+    source: "Oscar Wilde",
+    quote: "Be yourself; everyone else is already taken",
+    },
+  
+    {
+    source:"Bernard Baruch",
+    quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    },
+  
+    {
+    source: "Mae West",
+    quote: "You only live once, but if you do it right, once is enough."
+    },
+  
+    {
+    source: "Saint Augustine",
+    quote: "Do the heaven and earth then contain Thee, since Thou fillest them?",
+    citation: "Confessions of Augustine",
+    year: "AD 401"
+    },
+  
+    {
+    source: "Ralph Waldo Emerson",
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    },
+  ]
+  
+  function randomquote(quo){
+  let quote_body=[];
+  for(let i=0; i<quo.length; i+=1){
+    quote_body= push(quo[i].source);
+    
+  
+  }
+  return quote_body;
+  
+  }
+  
+  console.log(randomquote(quotes));
--- js/script.js
+++ js/script.js
@@ -11,47 +11,39 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-const quotes=[
+let quotes = [
   {
-  source: "Oscar Wilde",
-  quote: "Be yourself; everyone else is already taken",
-  }
-
-  {
-  source:"Bernard Baruch",
-  quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
-  }
-
-  {
-  source: "Mae West",
-  quote: "You only live once, but if you do it right, once is enough."
-  }
-
-  {
-  source: "Saint Augustine",
-  quote: "Do the heaven and earth then contain Thee, since Thou fillest them?",
-  citation: "Confessions of Augustine",
-  year: "AD 401"
-  }
-
-  {
-  source: "Ralph Waldo Emerson",
-  quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
-  }
-]
-
-function randomquote(quo){
-let quote_body=[];
-for(let i=0; i<quo.length; i+=1){
-  quote_body=.push(quo[i].source);
+      source: "Oscar Wilde",
+      quote: "Be yourself; everyone else is already taken",
+  },
   
-
-}
-return quote_body;
-
-}
-
-console.log(randomquote(products));
+  {
+      source:"Bernard Baruch",
+      quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+  },
+  
+  {
+      source: "Saint Augustine",
+      quote: "Do the heaven and earth then contain Thee, since Thou fillest them?",
+      citation: "Confessions of Augustine",
+      year: "AD 401"
+  }
+  
+  ];
+  
+  function getRandomQuote(quo){
+   let quoteBody = [];
+   let randomQuotes = Math.floor(Math.random() * quoteBody)
+   for (let i=0; i<quo.length; i+=1) {
+    quoteBody.push(quo[i].source);
+    quoteBody.push(quo[i].quote);
+    quoteBody.push(quo[i].citation);
+    quoteBody.push(quo[i].year);
+   }
+  return randomQuotes;
+  }
+  
+  console.log(getRandomQuote(quotes))
 
 
 
@@ -75,4 +67,4 @@ console.log(randomquote(products));
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document.getElementById('load-quote').addEventListener("click", printQuote, false);
